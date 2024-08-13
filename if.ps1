ab363fc2020ea79f4edceafe5a8ed0659cc4a972
marcos <git.conninck@gmail.com>
2024-08-13 10:20:57 -0300

    new file:   IfExample.ps1 	modified:   if.ps1

--- if.ps1
+++ if.ps1
@@ -1,20 +1,23 @@
 Clear-Host
 
-$Day = 'Sunday'
+'What day is it? '
+$Day = Read-Host
+
 
 if ($Day -eq 'Monday'){
-  'On Monday you eat Bami Goreng'
+    'On '+ $Day +' you eat Bami Goreng'
+    $Day = 'Tuesday' # Don't work, because the block of statement stoped here
 }
 
 elseif ($Day -eq 'Tuesday') {
-  'On Tuesday you eat Rendang'
-  'Cause you like that'
+    'On '+ $Day +' you eat Rendang'
+    'Cause you like that'
 }
 elseif ($Day -eq 'Wednesday') {
-  'On Wednesday you eat tomatoes'
+    'On '+ $Day +' you eat tomatoes'
 }
 else {
-  'I DONT CARE'
+    'I DONT CARE'
 }
 
 'Scipt stoped'
